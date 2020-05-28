4cb5dc6c22f10460ffedc9d5172d94435d746fda
TaylonGameta <taylon.gameta@outlook.com.br>
2020-05-28 17:11:22 -0300

    login function

--- model/Usuario.php
+++ model/Usuario.php
@@ -7,6 +7,38 @@
     */
 
     class Usuario{
+
+        public $email;
+        public $password;
+
+        private $conn;
+
+        public function __construct($db){
+            $this->conn = $db;
+        }
+
+        public function login(){
+            $stmt = $this->conn->prepare("SELECT * FROM usuario WHERE email = :email AND password = :password");
+            $stmt->execute(array('email' => $this->email, 'password' => $this->password));
+
+            $rowLength = $stmt->rowCount();
+
+            if($rowLength > 0){
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                $id = $result['id'];
+
+                $token = new Token();
+                $newToken = $token->create($id);
+                echo $newToken;
+            }
+            else{
+                $message = ['error'=> 'user not found'];
+                echo json_encode($message);
+                exit();
+            }
+            
+            
+        }   
     
         public function auth($userToken){
             $part = explode(" ", $userToken);
