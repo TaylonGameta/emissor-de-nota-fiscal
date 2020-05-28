0c24fc6cc9674e727964e9ac19eb9ad840cde11a
TaylonGameta <taylon.gameta@outlook.com.br>
2020-05-28 17:15:24 -0300

    login endpoint

---
+++ public/api/usuario/login.php
@@ -0,0 +1,26 @@
+<?php
+    include_once "../../../model/Usuario.php";
+    include_once "../../../config/Database.php";
+
+
+    //headers 
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+
+    //database connection
+    $database = new Database();
+    $conn = $database->createConnection();
+   
+    //get values from json request raw
+    $data = json_decode(file_get_contents("php://input"));
+
+
+    //user stuff
+    $usuario = new Usuario($conn);
+    $usuario->email = $data->email;
+    $usuario->password = $data->password;
+
+    $usuario->login();
+
+?>
