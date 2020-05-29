330d8ee128b754c7ec8ef3366c100cd59f8e0055
TaylonGameta <taylon.gameta@outlook.com.br>
2020-05-29 19:42:38 -0300

    produto endpoints

---
+++ public/api/produto/getall.php
@@ -0,0 +1,32 @@
+<?php
+    include_once "../../../config/Database.php";
+    include_once "../../../model/Produto.php";
+    include_once "../../../model/Usuario.php";
+    include_once "../../../model/Token.php";
+
+    // headers
+    header("Content-Type: application/json");
+
+    // database connection
+    $database = new Database();
+    $conn = $database->createConnection();
+
+    // auth
+    $headers = apache_request_headers();
+    $token = $headers['Authorization'];
+
+    $usuario = new Usuario($conn);
+    $auth = $usuario->auth($token);
+
+    //get values from json request raw
+    $data = json_decode(file_get_contents("php://input"));
+
+    /*
+    *** creating a produto class to get all
+    */
+    $produto = new Produto($conn);
+    
+    $produto->getAll();
+
+
+?>
