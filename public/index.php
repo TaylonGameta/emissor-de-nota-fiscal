72c2f81a4f9534054245ab8278b645d71bf827a2
TaylonGameta <taylon.gameta@outlook.com.br>
2020-05-30 20:16:52 -0300

    homepage

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+        
+    </head>
+
+    <body>
+        <div class="spinner-container">
+            <p>NFeasy</p>
+            <div class="spinner">
+                <div class="dot-1"></div>
+                <div class="dot-2"></div>
+                <div class="dot-3"></div>
+            </div>
+        </div>
+    </body>
+</html>
